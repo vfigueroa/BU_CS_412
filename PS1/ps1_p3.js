bd6f6989baf41c5e8ab2cdf91a6fb1d411f1aab8
Victor Figueroa <vfigueroa@dhcp-wifi-8021x-155-41-88-247.bu.edu>
2021-09-23 13:18:24 -0400

    Updated PS1

--- PS1/ps1_p3.js
+++ PS1/ps1_p3.js
@@ -1,5 +1,15 @@
 const func = (string, lambda) => {return lambda(string)};
 
-//My lambda function uses replaceAll to add a deliminator so that the original sting is preserved when we split
-
+// Part 1
 console.log(func('supercalifragilisticexpialidocious', s => s.replaceAll('c', '&$%@c').split('&$%@')));
+
+// Part 2
+console.table(func('supercalifragilisticexpialidocious',  s => {
+    let object = {
+        originalString: s,
+        modifiedString: s.replaceAll('a', 'A'),
+        numberReplaced: s.replaceAll('a', 'A').split('A').length - 1,
+        length: s.replaceAll('a', 'A').length
+    }
+    return object
+}));
